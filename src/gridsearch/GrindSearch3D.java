c10aca2fefe5306657fd9eda9b26d7315baf5947
Jussi Karppinen <misterkarppinen@gmail.com>
2017-04-05 23:54:33 +0300

    ...

--- src/gridsearch/GrindSearch3D.java
+++ src/gridsearch/GrindSearch3D.java
@@ -2,8 +2,10 @@ package gridsearch;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Predicate;
 
 public class GrindSearch3D {
@@ -19,16 +21,12 @@ public class GrindSearch3D {
 			
 			List<Integer> result = search(node, new HashMap<Integer, List<Integer>>(), 0, maxLevels, isGoal);
 			if (result != null){
-				
-//				System.out.println("Nodes visited:"+visits);
-
 				return result;
 				
 			}
 			
 		}
 		
-//		System.out.println("Nodes visited:"+visits);
 		return null;
 	}
 	
@@ -54,9 +52,9 @@ public class GrindSearch3D {
 		
 		for (Node node: root.getAdjacents(level)){
 			
-			if (visited.get(node.getGridIndex()) == null || visited.get(node.getGridIndex()).indexOf(node.getId()) == -1){
+			if (visited.get(node.getGridIndex()) == null || visited.get(node.getGridIndex()).indexOf(node.getId()) == -1){	
 				
-				List<Integer> result = search(node, new HashMap<Integer, List<Integer>>(visited), level+1, maxLevels, isGoal);
+				List<Integer> result = search(node, createCopy(visited), level+1, maxLevels, isGoal);
 				if (result != null){
 					return result;
 				}
@@ -67,6 +65,17 @@ public class GrindSearch3D {
 		
 		
 		return null;
+	}
+	
+	
+	private Map<Integer, List<Integer>> createCopy(Map<Integer, List<Integer>> m){
+		HashMap<Integer, List<Integer>> temp = new HashMap<Integer, List<Integer>>();
+		Iterator<Entry<Integer, List<Integer>>> it = m.entrySet().iterator();
+		while(it.hasNext()){
+			Entry<Integer, List<Integer>> e = it.next();
+			temp.put(e.getKey(), new ArrayList<Integer>(e.getValue()));
+		}
+		return temp;
 	}
 	
 }
